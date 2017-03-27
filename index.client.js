d2781b3552a2454b7d676b68afa812853951b062
DESKTOP-CQ6CRUJ\Rostyslay Kanafotsky <03061998jd@gmail.com>
2017-03-27 17:53:38 +0300

    basic save function added

---
+++ index.client.js
@@ -0,0 +1,22 @@
+function save() {
+    // fetch(window.location.host + '/api/service/' + service.id, {
+    //     method: 'post',
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify(service)
+    // }).then(json).then(function (data) {
+    //         console.log('Request succeeded with JSON response', data);
+    //     })
+    //     .catch(function (error) {
+    //         console.log('Request failed', error);
+    //     });
+}
+
+function init() {
+    //service = service;
+    console.log("services",services);
+ 
+}
+
+init();
--- index.js
+++ index.js
@@ -28,24 +28,27 @@ app.get('/sort/:prop', function (req, res) {
     let sortedServices = services.sort(function (a, b) {
 
         if (a[prop] > b[prop]) {
-            console.log("1", a[prop], ">", b[prop])
             return 1;
           }
           if (a[prop] < b[prop]) {
-            console.log("-1", a[prop], "<", b[prop])
-            return -1;
+              return -1;
           }
           // a должно быть равным b
-        console.log("0", a[prop], "=", b[prop])
         return 0;
     })
     res.render('index', {services: sortedServices})
 })
+
+
 app.get('/:id', function (req, res) {
     let id = req.params.id
     service = services.find((item)=> item.id === id)
     res.render('service', {service: service})
 })
+
+app.put('/api/service/:id', function (req, res) {
+
+});
 
 var server = app.listen(3000, function () {
     console.log('Server running at http//localhost:' + server.address().port)
